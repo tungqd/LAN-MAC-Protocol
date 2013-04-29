e6283ea4740668c5c344b3c1e9c4eba3dcd9640b
Tung Dang <tungqd90@yahoo.com>
2013-04-28 18:26:56 -0700

    Part A is done

--- simulation.java
+++ simulation.java
@@ -4,6 +4,7 @@
  */
 package lmp;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 
@@ -13,12 +14,12 @@ import java.util.ArrayList;
  */
 public class simulation {
     //Number of stations
-    public static int N = 5;
+    public static int N = 20;
     //number of runs
     public static int RUNCOUNT = 100;
     
     public static station[] S = new station[N];
-    
+    static DecimalFormat numberFormat = new DecimalFormat("#.00");
     
     public static ArrayList run(int N) {
         
@@ -58,61 +59,24 @@ public class simulation {
         return results;
     }
     
-    public static double avg(ArrayList arrivalTimes)
-    {
-        double total = 0;
-        for(int i = 0;i <= arrivalTimes.size()-1; i++)
-        {
-            total =  total + (Integer) arrivalTimes.get(i);
-        }
-        return total / arrivalTimes.size(); // returns the average
-    }
-    
     public static void main(String argv[]) {
-       
-         
-        int i, runsum = 0;
-        int hostA = 0;
-        int hostB = 0;
-        int hostC = 0;
-        int hostD = 0;
-        int hostE = 0;
+             
+        int i;
+        int[] host = new int[N];
         
         for (i=0; i<RUNCOUNT; i++) {
             ArrayList oneRun = run(N);
             for (int j=0; j<oneRun.size(); j++) {
-                
-            	int temp = (Integer) oneRun.get(j);
-            	
-                if(j == 0)
-                {
-                	hostA = hostA + temp;
-                }
-                if(j == 1)
-                {
-                	hostB = hostB + temp;
-                }
-                if(j == 2)
-                {
-                	hostC = hostC + temp;
-                }
-                if(j == 3)
-                {
-                	hostD = hostD + temp;
-                }
-                if(j == 4)
-                {
-                	hostE = hostE + temp;
-                }
+               // System.out.println(oneRun.get(j));
+            	host[j] += (Integer) oneRun.get(j);          
             }
         }
-        System.out.println(hostA/RUNCOUNT);
-        System.out.println(hostB/RUNCOUNT);
-        System.out.println(hostC/RUNCOUNT);
-        System.out.println(hostD/RUNCOUNT);
-        System.out.println(hostE/RUNCOUNT);
-        
-        //System.out.println("runsum =" + runsum + "RUNCOUNT = " +
-        //        RUNCOUNT + "average= " + ((double) (runsum / RUNCOUNT)));
+        double[] avg = new double[N];
+       for (i=0; i<N; i++) {
+           avg[i] = ((double) host[i]/RUNCOUNT) ;
+           System.out.println("Average delayed time for " + (i+1) + " is: " 
+                   + numberFormat.format(avg[i]));
+       }
+
     }
 }
